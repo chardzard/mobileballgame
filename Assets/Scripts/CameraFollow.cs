4b7c27933ed58f404a9f7bd77bb68f00f8b2de6e
chardzard <richard.sage28@gmail.com>
2016-06-25 14:32:24 -0300

    Added materials and scripts

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    Vector3 offset;
+
+    // Use this for initialization
+    void Start() {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+        transform.position = target.position + offset;
+    }
+}
